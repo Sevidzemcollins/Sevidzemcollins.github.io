94eb7347618f3c5a2aea98e841dac4386168f43f
Sevidzemcollins <122789703+Sevidzemcollins@users.noreply.github.com>
2023-05-30 14:26:16 +0100

    changed defaolt loading page

--- portfolio.js
+++ portfolio.js
@@ -39,7 +39,7 @@ const switchPage = (newPage) => {
 
 document.addEventListener("DOMContentLoaded", () => {
   //set the home page as the default page
-  switchPage("projects");
+  switchPage("home");
 
   //add logic for toggling the menu
   const menu = document.querySelector(".menu");
